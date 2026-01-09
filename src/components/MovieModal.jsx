b380ccbd960ab61737e402b84f7a768c09c53a07
Aman <dmsammanuel@gmail.com>
2026-01-09 12:27:59 +0300

    replaced the modal with headlessUI's component

--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -4,24 +4,32 @@ import { useState } from "react";
 
 const MovieModal = ({ movie, isModalOpen, setIsModalOpen }) => {
     return (
-        <Dialog
-            open={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-            className="relative z-50"
-        >
-            <DialogBackdrop className="fixed inset-0 bg-black/80" />
+        <AnimatePresence>
+            {open && (<Dialog
+                open={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                className="relative z-50"
+                as={motion.div}
+                exit={{ opacity: 0 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+            >
+                <DialogBackdrop className="fixed inset-0 bg-black/80" />
 
-            <div className="fixed inset-0 flex items-center justify-center p-4 overflow-y-auto">
-                <DialogPanel className="bg-[#0F0D23] rounded-xl p-6 max-w-md w-full">
-                    <DialogTitle className="text-2xl font-bold">{movie?.title}</DialogTitle>
+                <div className="fixed inset-0 p-4 overflow-y-auto">
+                    <DialogPanel className="bg-[#0F0D23] rounded-xl p-6 max-w-md w-full mt-0">
+                        <DialogTitle className="text-2xl font-bold">{movie?.title}</DialogTitle>
 
-                    <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : './no-movie.png'} alt="Movie" />
-                    <p className="mt-4 text-gray-300">overview: {movie.overview}</p>
+                        <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : './no-movie.png'} alt="Movie" />
+                        <p className="mt-4 text-gray-300">overview: {movie.overview}</p>
 
-                    <button onClick={() => setIsModalOpen(false)} className="mt-4 px-4 py-2 bg-purple-600 text-white rounded cursor-pointer">Close</button>
-                </DialogPanel>
-            </div>
-        </Dialog>
+                        <button onClick={() => setIsModalOpen(false)} className="mt-4 px-4 py-2 bg-purple-600 text-white rounded cursor-pointer">Close</button>
+                    </DialogPanel>
+                </div>
+            </Dialog>
+            )}
+
+        </AnimatePresence>
     );
 };
 

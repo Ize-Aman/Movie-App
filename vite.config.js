66120bdd3144472e5c557b97801c006210cebc67
Aman <dmsammanuel@gmail.com>
2026-01-30 21:21:59 +0300

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
     }
   },
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/Movie-App",
   server: {
     watch: {
       usePolling: true

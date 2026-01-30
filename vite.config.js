818c280016a2ebcf19686448abe33a8623ededad
Ammanuel Tesfahun <156339946+Ize-Aman@users.noreply.github.com>
2026-01-30 22:15:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,8 +16,5 @@ export default defineConfig({
   },
   plugins: [react(), tailwindcss()],
   base: process.env.VITE_BASE_PATH || "/Movie-App",
-  build: {
-    chunkSizeWarningLimit: 1000, // Set the limit in KiB (e.g., 1000 kB)
-  },
 })
 
